bfa2a96d08d673ecea9032e67a18ec46e3d2d34c
JoshuaD9907 <45548505+JoshuaD9907@users.noreply.github.com>
2020-12-02 13:49:28 -0700

    More processing compatability updates

--- final_project_ccs/Actions.c
+++ final_project_ccs/Actions.c
@@ -2,15 +2,15 @@
 #include "Emotion.h"
 #include "Movement.h"
 #include "Piezo.h"
+#include "oled.h"
+#include "bitmaps.h"
 
 /**
  * Actions.c
  */
 void Actions(){
     string currentEmotion = Emotion(); //Emotion
-    Movement(currentEmotion); //Movement takes current emotion and acts accordingly
-    if(currentEmotion == “bored” || currentEmotion == “happy” || currentEmotion == “surprised”){ //If current emotion is one of the emotions that outputs sound
-        piezo_noise(currentEmotion);
-        oled_draw(currentEmotion);
-    }
+    //Movement(currentEmotion); //Movement takes current emotion and acts accordingly
+    piezo_noise(currentEmotion);
+    oled_display(currentEmotion);
 }
--- final_project_ccs/Emotion.c
+++ final_project_ccs/Emotion.c
@@ -7,8 +7,9 @@
  */
 string Emotion(){
     string outputEmotion = “neutral”;
-
-    boolean plannedMovement = IMU(); //Get whether the movement is going as planned from the IMU
+    boolean plannedMovement = true;
+    
+    //plannedMovement = IMU(); //Get whether the movement is going as planned from the IMU
     if(pet_check() == 1){ //If being pet, set to happy
         outputEmotion = “happy”;
     }
